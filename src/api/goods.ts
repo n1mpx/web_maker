0d64574ebd490fcc544e5c3577d5ae9a2f6abf92
Кисляк Игорь <Kislyakim@gmail.com>
2025-06-04 16:42:53 +0500

    11

--- src/api/goods.ts
+++
@@ -1,8 +0,0 @@
-import api from './axios';
-
-
-export const getGoods = () => api.get('/catalog/');
-export const getGood = (id: string) => api.get(`/catalog/${id}`);
-export const createGood = (data: unknown) => api.post('/goods', data);
-export const updateGood = (id: string, data: unknown) => api.patch(`/goods/${id}`, data);
-export const deleteGood = (id: string) => api.delete(`/goods/${id}`);
